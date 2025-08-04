2fd42c4fe06ea2644e6292a349623ce317c69170
omardoescode <omardoescode@gmail.com>
2025-08-04 08:01:08 +0300

    feat: Use bincode serialization engine instead of building our own

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
+use SSTables::memtable::MemTable;
+use bincode::{Decode, Encode};
+
+#[derive(Encode, Decode, Clone)]
+struct User {
+    username: String,
+    password: String,
+}
 fn main() {
-    println!("Hello, world!");
+    let mut memtable = match MemTable::<User>::new() {
+        Ok(val) => val,
+        Err(err) => {
+            panic!("Failed to create a new memtable: {err:?}");
+        }
+    };
+
+    memtable.insert(
+        "admin".to_string(),
+        User {
+            username: "admin".to_string(),
+            password: "1234".to_string(),
+        },
+    );
+
+    memtable.insert(
+        "admin".to_string(),
+        User {
+            username: "admin".to_string(),
+            password: "1234".to_string(),
+        },
+    );
+
+    memtable.insert(
+        "omar".to_string(),
+        User {
+            username: "omar".to_string(),
+            password: "HelloWorld".to_string(),
+        },
+    );
 }
--- src/memtable.rs
+++ src/memtable.rs
@@ -1,22 +1,17 @@
-use std::fs::{File, OpenOptions};
+use bincode::{Decode, Encode, config};
+use std::fs::{File, OpenOptions, create_dir_all};
 use std::io::{BufReader, Error, ErrorKind, Read, Result as IOResult, Write};
 
 use rbtree::RBTree;
 
-pub trait Serializable {
-    fn serialize(&self) -> Vec<u8>;
-    fn deserialize(data: &[u8]) -> Self
-    where
-        Self: Sized;
-}
-
-pub struct MemTable<T: Serializable> {
+pub struct MemTable<T: Decode<()> + Encode + Clone> {
     tree: RBTree<String, T>,
     log: MemTableLog,
 }
 
-impl<T: Serializable> MemTable<T> {
-    fn new() -> IOResult<MemTable<T>> {
+impl<T: Decode<()> + Encode + Clone> MemTable<T> {
+    pub fn new() -> IOResult<MemTable<T>> {
+        create_dir_all("logs")?;
         let file = OpenOptions::new()
             .create(true)
             .append(true)
@@ -27,14 +22,13 @@ impl<T: Serializable> MemTable<T> {
         })
     }
 
-    fn build_from(path: &str) -> IOResult<MemTable<T>> {
+    pub fn build_from(path: &str) -> IOResult<MemTable<T>> {
         let mut reader = MemTableLogReader::open(path)?;
         let mut memtable = MemTable::new()?;
 
         while let Some(op) = reader.next_op()? {
             match op {
                 LogOperation::Insert { key, value } => {
-                    let value = T::deserialize(&value);
                     memtable.insert(key, value);
                 }
                 LogOperation::Delete { key } => {
@@ -46,32 +40,32 @@ impl<T: Serializable> MemTable<T> {
         Ok(memtable)
     }
 
-    fn insert(&mut self, key: String, value: T) -> IOResult<()> {
+    pub fn insert(&mut self, key: String, value: T) -> IOResult<()> {
         self.log.append(LogOperation::Insert {
             key: key.clone(),
-            value: value.serialize(),
+            value: value.clone(),
         })?;
         self.tree.insert(key, value);
         Ok(())
     }
 
-    fn delete(&mut self, key: String) -> IOResult<bool> {
+    pub fn delete(&mut self, key: String) -> IOResult<bool> {
         let result = self.tree.remove(&key).is_some();
         if result {
-            self.log.append(LogOperation::Delete { key })?;
+            self.log.append(LogOperation::<T>::Delete { key })?;
         }
         Ok(result)
     }
 
-    fn get(&self, key: &String) -> Option<&T> {
+    pub fn get(&self, key: &String) -> Option<&T> {
         self.tree.get(key)
     }
 
-    fn len(&self) -> usize {
+    pub fn len(&self) -> usize {
         self.tree.len()
     }
 
-    fn is_empty(&self) -> bool {
+    pub fn is_empty(&self) -> bool {
         self.tree.is_empty()
     }
     pub fn iter(&self) -> rbtree::Iter<String, T> {
@@ -82,8 +76,9 @@ impl<T: Serializable> MemTable<T> {
     }
 }
 
-enum LogOperation {
-    Insert { key: String, value: Vec<u8> },
+#[derive(Encode, Decode, PartialEq, Debug)]
+enum LogOperation<T: Encode + Decode<()>> {
+    Insert { key: String, value: T },
     Delete { key: String },
 }
 
@@ -96,21 +91,11 @@ impl MemTableLog {
         MemTableLog { file }
     }
 
-    fn append(&mut self, opt: LogOperation) -> IOResult<()> {
-        match opt {
-            LogOperation::Insert { key, value } => {
-                self.file.write_all(&[1])?;
-                self.file.write_all(&(key.len() as u32).to_le_bytes())?;
-                self.file.write_all(key.as_bytes())?;
-                self.file.write_all(&(value.len() as u32).to_le_bytes())?;
-                self.file.write_all(&value)?;
-            }
-            LogOperation::Delete { key } => {
-                self.file.write_all(&[2])?;
-                self.file.write_all(&(key.len() as u32).to_le_bytes())?;
-                self.file.write_all(key.as_bytes())?;
-            }
-        }
+    fn append<T: Encode + Decode<()>>(&mut self, opt: LogOperation<T>) -> IOResult<()> {
+        let Ok(decoded) = bincode::encode_to_vec(opt, config::standard()) else {
+            return Err(Error::new(ErrorKind::InvalidInput, "Failed to encode data"));
+        };
+        self.file.write_all(&decoded)?;
         self.file.flush()?;
         Ok(())
     }
@@ -130,36 +115,12 @@ impl MemTableLogReader<File> {
 }
 
 impl<R: Read> MemTableLogReader<R> {
-    fn next_op(&mut self) -> IOResult<Option<LogOperation>> {
-        let mut op = [0u8; 1];
-        if self.reader.read(&mut op)? == 0 {
-            return Ok(None);
-        }
-
-        let mut key_len = [0u8; 4];
-        self.reader.read_exact(&mut key_len)?;
-        let key_len = u32::from_le_bytes(key_len) as usize;
-
-        let mut key = vec![0u8; key_len];
-        self.reader.read_exact(&mut key)?;
-        let key = String::from_utf8_lossy(&key).to_string();
-
-        if op[0] == 1 {
-            let mut value_len = [0u8; 4];
-            self.reader.read_exact(&mut value_len)?;
-            let value_len = u32::from_le_bytes(value_len) as usize;
-
-            let mut value = vec![0u8; value_len];
-            self.reader.read_exact(&mut value)?;
-
-            Ok(Some(LogOperation::Insert { key, value }))
-        } else if op[0] == 2 {
-            Ok(Some(LogOperation::Delete { key }))
-        } else {
-            Err(Error::new(
-                ErrorKind::InvalidData,
-                format!("Invalid log opcode: {}", op[0]),
-            ))
+    fn next_op<T: Decode<()> + Encode>(&mut self) -> IOResult<Option<LogOperation<T>>> {
+        let result = bincode::decode_from_std_read(&mut self.reader, config::standard());
+        match result {
+            Ok(op) => Ok(Some(op)),
+            Err(bincode::error::DecodeError::UnexpectedEnd { .. }) => Ok(None),
+            Err(e) => Err(Error::new(ErrorKind::Other, e)),
         }
     }
 }
